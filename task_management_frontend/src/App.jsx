d10984ea65e4cf53eaddc0e49c5d696f349c8e4a
dhruv sawant <dhruvsawant1811@gmail.com>
2025-03-24 20:06:52 +0530

    Updated some routing

--- task_management_frontend/src/App.jsx
+++ task_management_frontend/src/App.jsx
@@ -1,6 +1,7 @@
+// src/App.jsx
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/layout/Layout";
+import Layout from "./components/Layout/Layout"; // Fixed import casing
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/Signup/Signup";
@@ -10,12 +11,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />  {/* Default page is now Login */}
+        <Route path="/" element={<Login />} />  {/* Default page is Login */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Layout />}>
-          <Route index element={<Dashboard />} />
+        <Route path="/" element={<Layout />}>  {/* Layout wraps app pages */}
+          <Route path="dashboard" element={<Dashboard />} />
           <Route path="tasks" element={<Tasks />} />
+          <Route index element={<Dashboard />} />  {/* / shows Dashboard */}
         </Route>
       </Routes>
     </BrowserRouter>
