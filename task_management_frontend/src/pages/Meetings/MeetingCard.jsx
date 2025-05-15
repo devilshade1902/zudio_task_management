c5e622f4bc591f98a7e829698534e9b74991fb22
lipika1911 <lipikaarya1723@gmail.com>
2025-05-15 12:51:12 +0530

    some meeting updations

--- task_management_frontend/src/pages/Meetings/MeetingCard.jsx
+++ task_management_frontend/src/pages/Meetings/MeetingCard.jsx
@@ -203,8 +203,7 @@ const MeetingCard = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const token = localStorage.getItem('token');
-      const roomName = `${selectedMeeting.title}-${Date.now()}`;
+      const roomName = `${selectedMeeting.title}`;
       const participantEmails = taskUsers
         .filter(user => selectedParticipants.includes(user._id))
         .map(user => user.email);
